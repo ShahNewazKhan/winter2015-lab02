570bf7f53004be89f590e488e31e5b13c0238f2a
Shah <shahnewazk@gmail.com>
2015-01-18 02:32:26 -0800

    Commented About.php & Gallery.php

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -1,6 +1,17 @@
 <?php
+/**
+ *
+ * @package	COMP 4711 Lab 2
+ * @author	Shah Newaz Khan
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ */
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') OR exit ('No direct script access allowed');
+
+/**
+ *Controller for applicaion/views/gallery.php
+ *
+ */
 
 class About extends CI_Controller {
 
--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -1,7 +1,17 @@
 <?php
+/**
+ *
+ * @package	COMP 4711 Lab 2
+ * @author	Shah Newaz Khan
+ * @license	http://opensource.org/licenses/MIT	MIT License
+ */
 
 defined('BASEPATH') OR exit ('No direct script access allowed');
 
+/**
+ *Controller for applicaion/views/gallery.php
+ *
+ */
 class Gallery extends CI_Controller 
 {
 	public function index()
